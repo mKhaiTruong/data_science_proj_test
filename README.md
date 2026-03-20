08b0051a6e1b1dc4f0133e94dab1bc80d8607e1f
mKhaiTruong <101619139+mKhaiTruong@users.noreply.github.com>
2026-03-21 00:44:49 +0700

    Complete data ingestion pipeline

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-## First commit - test project
+# End 2 end data science project
+
+## Workflow:
+
+1. Data ingestion and validate data
+2. Data transformation
+3. Train & eval models
+4. ??
+
+## Steps:
+
+1. update config.yaml
+2. update schema.yaml
+3. update params.yaml
+4. update the entity
+5. update the configuration manager in src config
+6. update the components
+7. update the pipeline
+8. update main.py
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from src.data_science import logger
+from src.data_science.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 
-logger.info("Hello World and Custom Logger")
+STAGE_NAME = "Data Ingestion stage"
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+        raise e
