3fc9ebc9b3a4133bc6047daeb1128c95dab60e70
mKhaiTruong <101619139+mKhaiTruong@users.noreply.github.com>
2026-03-21 22:36:16 +0700

    Data validation completed

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from src.data_science import logger
 from src.data_science.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.data_science.pipeline.data_validation_pipeline import DataValidationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
         obj = DataIngestionPipeline()
         obj.initiate_data_ingestion()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataValidationPipeline()
+        obj.initiate_data_validation()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
         raise e
